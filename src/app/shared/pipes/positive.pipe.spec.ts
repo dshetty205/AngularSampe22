ee28953fbd72196d851277a2e1d185ae12a75d3a
dshetty205 <daya.s.d@gmail.com>
2021-02-23 18:46:13 -0500

    1 file modified, 6 deleted

--- src/app/shared/pipes/positive.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PositivePipe } from './positive.pipe';
-
-describe('PositivePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PositivePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
